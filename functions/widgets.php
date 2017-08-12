aadc1b61535470449e5594880fee2014ebb17253
Zoltán Adamek <djscorchio@gmail.com>
2017-08-12 21:48:09 +0200

    Adds front page widget area, removes dummy content.

--- functions/widgets.php
+++ functions/widgets.php
@@ -23,5 +23,15 @@ function bulmapress_widgets_init() {
 		'before_title'  => '<h2 class="widget-title is-bold">',
 		'after_title'   => '</h2>',
 		) );
+
+	register_sidebar(array(
+	    'name' => esc_html__('Front page widget area', 'bulmapress'),
+        'id' => 'frontpage-sidebar',
+        'description' => esc_html__('Add widgets here to make them appear on the front page.', 'bulmapress'),
+        'before_widget' => '',
+        'after_widget'  => '',
+        'before_title'  => '',
+        'after_title'   => '',
+    ));
 }
 add_action( 'widgets_init', 'bulmapress_widgets_init' );
--- home.php
+++ home.php
@@ -18,49 +18,17 @@
 <div id="primary" class="content-area">
 	<main id="main" class="site-main wrapper" role="main">
 		<div class="container">
-			<div class="columns">
-				<div class="column tile is-ancestor">
-					<div class="tile is-vertical is-parent is-6">
-						<div class="tile is-child notification is-primary">
-							<h2 class="title">Események</h2>
-						</div>
-						<div class="tile is-child notification is-warning">
-							<h2 class="title">Cikkek</h2>
-						</div>
-					</div>
-					<div class="tile is-vertical is-parent is-6">
-						<div class="tile is-child notification is-danger">
-							<h2 class="title">VAKstars</h2>
-						</div>
-						<div class="tile is-child notification is-success">
-							<h2 class="title">Galléria</h2>			
-						</div>
-					</div>
-				</div>	
-			</div>
+            <div class="columns">
+                <div class="column">
+                    <?php if ( is_active_sidebar( 'frontpage-sidebar' ) ) : ?>
+                        <div id="front-page-widgets" >
+                            <?php dynamic_sidebar( 'frontpage-sidebar' ); ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </div>
 		</div>
 
-
-
-
-		<?php if ( have_posts() ) : ?>
-			<div class="container">
-				<div class="columns is-multiline">
-					<?php while ( have_posts() ) : the_post(); ?>
-						<div class="column is-one-third">
-							<?php get_template_part( 'template-parts/content', 'post' ); ?>
-						</div>
-					<?php endwhile; ?>
-				</div>
-			</div>
-			<div class="section pagination">
-				<div class="container">
-					<?php the_posts_pagination(); ?>
-				</div>
-			</div>
-		<?php else : ?>
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-		<?php endif; ?>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
