eaeeab4d1693ed4b167ec00df6ef853b5a99be8e
Zoltán Adamek <djscorchio@gmail.com>
2017-08-12 22:58:03 +0200

    Improves markup of the post page.

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -9,52 +9,37 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('box'); ?>>
-	<?php if ( has_post_thumbnail() ): ?>
-		<div class="post-image">
-			<figure class="image is-16by9">
-				<?php if ( is_single() ) : ?>
-					<?php the_post_thumbnail(); ?>
-				<?php else : ?>
-					<?php the_post_thumbnail('widget'); ?>
-				<?php endif; ?>
-			</figure>
-		</div>
-	<?php endif; ?>
-	<div class="post-content">
-		<div class="media">
-			<header class="media-content">
-				<?php if ( is_single() ) : ?>
-					<?php bulmapress_the_title('is-2', false); ?>
-				<?php else : ?>
-					<?php bulmapress_the_title('is-3'); ?>
-				<?php endif; ?>
+    <header>
+        <?php if ( is_single() ) : ?>
+            <?php bulmapress_the_title('is-2', false); ?>
+        <?php else : ?>
+            <?php bulmapress_the_title('is-3'); ?>
+        <?php endif; ?>
 
-				<?php if ( 'post' === get_post_type() ) : ?>
-					<div class="subtitle is-6">
-						<?php bulmapress_posted_on(); ?>
-					</div><!-- .entry-meta -->
-				<?php endif; ?>
-			</header><!-- .entry-header -->
-		</div>
-		<div class="content entry-content">
-		<?php if ( is_single() ) : ?>
-			<?php the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'bulmapress' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				) );
+        <?php if ( 'post' === get_post_type() ) : ?>
+            <div class="subtitle is-6">
+                <?php bulmapress_posted_on(); ?>
+            </div><!-- .entry-meta -->
+        <?php endif; ?>
+    </header><!-- .entry-header -->
+    <div class="content entry-content">
+    <?php if ( is_single() ) : ?>
+        <?php the_content( sprintf(
+            /* translators: %s: Name of current post. */
+            wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'bulmapress' ), array( 'span' => array( 'class' => array() ) ) ),
+            the_title( '<span class="screen-reader-text">"', '"</span>', false )
+            ) );
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bulmapress' ),
-				'after'  => '</div>',
-				) );
-				?>
-		<?php else : ?>
-			<?php the_excerpt(); ?>
-		<?php endif; ?>
-            <div class="content entry-footer">
-                <small><?php bulmapress_entry_footer(); ?></small>
-            </div><!-- .entry-footer -->
-        </div><!-- .entry-content -->
-    </div>
+        wp_link_pages( array(
+            'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bulmapress' ),
+            'after'  => '</div>',
+            ) );
+            ?>
+    <?php else : ?>
+        <?php the_excerpt(); ?>
+    <?php endif; ?>
+        <div class="content entry-footer">
+            <small><?php bulmapress_entry_footer(); ?></small>
+        </div><!-- .entry-footer -->
+    </div><!-- .entry-content -->
 </article><!-- #post-## -->
